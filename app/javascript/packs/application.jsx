bee89b1adf2ecee7fff3936b2d626ceb15ad7622
roy yoo <royyoo123@gmail.com>
2021-04-03 22:03:03 -0700

    testing rails and react

---
+++ app/javascript/packs/application.jsx
@@ -0,0 +1,12 @@
+import "../src";
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	ReactDOM.render(<App />, document.getElementById('root'));
+
+})
